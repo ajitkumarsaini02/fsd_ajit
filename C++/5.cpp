5e15552ed55f0d8ef7045b41aa9867a8428b2df1
ajitkumarsaini02 <ajitkumarsaini02@gmail.com>
2025-08-28 22:38:47 +0530

    ...

--- C++/5.cpp
+++ C++/5.cpp
@@ -1,17 +1,15 @@
+//Input sorted array (-3,-2,-1,0,2,4) and sort on the basis of equation Ax2+Bx+C, A=-1,B=-4,c=1
 #include <iostream>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
-const int A = -1;
-const int B = -4;
-const int C = 1;
-
 static bool cmp_quadratic(int x, int y) {
-    int val_x = (int)A * x * x + (int)B * x + C;
-    int val_y = (int)A * y * y + (int)B * y + C;
-    return val_x < val_y;
+     int A = -1;
+     int B = -4;
+     int C = 1;
+    return ((A*x*x + B*x + C)<(A*y*y + B*y + C));
 }
 
 int main() {
